ec88d29d358f1a121e02d620439ee3ce1036ebc8
Darshan S <darshandd1103@gmail.com>
2026-06-04 17:05:08 +0530

    Added routing for admin and feedback page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
-import "./App.css";
-//import CustomerFeedback from "./pages/CustomerFeedback";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import CustomerFeedback from "./pages/CustomerFeedback";
 import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
-  return <AdminDashboard />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/feedback" element={<CustomerFeedback />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+
+        <Route
+          path="*"
+          element={<Navigate to="/feedback" />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
